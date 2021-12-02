7f5d040afb9f8cd75aa32373cd60f85fc5c0ac73
Luis Fernando Illapa <moxb@outlook.es>
2021-12-02 08:20:51 -0500

    build(front): setup eslint

--- gendocs-frontend/src/App.tsx
+++ gendocs-frontend/src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-      </header>
-    </div>
-  )
-}
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Hello Vite + React!</p>
+            </header>
+        </div>
+    );
+};
 
-export default App
+export default App;
