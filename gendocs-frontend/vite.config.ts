4c1aa1307eaeef6c67935d6e15811d68609e37fe
Luis Fernando Illapa <moxb@outlook.es>
2021-12-02 21:35:54 -0500

    build(front): setup relative paths

--- gendocs-frontend/vite.config.ts
+++ gendocs-frontend/vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
+
+function srcPath(subdir) {
+    return path.join(__dirname, "src", subdir);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    plugins: [react()],
+    resolve: {
+        alias: {
+            components: srcPath("components/"),
+            pages: srcPath("pages/"),
+            hooks: srcPath("hooks/"),
+            contexts: srcPath("contexts/"),
+            layout: srcPath("layout/"),
+            models: srcPath("models/"),
+            services: srcPath("services/"),
+            utils: srcPath("utils/"),
+            providers: srcPath("providers/"),
+            reducers: srcPath("reducers/"),
+            routes: srcPath("routes/"),
+        },
+    },
 })
