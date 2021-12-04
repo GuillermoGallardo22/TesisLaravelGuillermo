4f7552df838c919439df25fab4e4e0b393c1a77c
Luis Fernando Illapa <moxb@outlook.es>
2021-12-04 15:22:40 -0500

    feat(front): add theme provider

--- gendocs-frontend/src/App.tsx
+++ gendocs-frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { ThemeProvider } from "@emotion/react";
+import { createTheme, CssBaseline } from "@mui/material";
 import PrivateLayout from "layout/PrivateLayout";
 import Login from "pages/public/Login";
 import AuthProvider from "providers/AuthProvider";
@@ -7,13 +9,24 @@ import {
 import PrivateRoute from "routes/PrivateRoute";
 import PublicRoute from "routes/PublicRoute";
 
+const mdTheme = createTheme({
+    palette: {
+        primary: {
+            main: "#7a1e19",
+        },
+    },
+});
+
 const App = () => {
     return (
-        <BrowserRouter>
-            <AuthProvider>
-                <AppBase />
-            </AuthProvider>
-        </BrowserRouter>
+        <ThemeProvider theme={mdTheme}>
+            <CssBaseline />
+            <BrowserRouter>
+                <AuthProvider>
+                    <AppBase />
+                </AuthProvider>
+            </BrowserRouter>
+        </ThemeProvider>
     );
 };
 
--- gendocs-frontend/src/layout/PrivateLayout.tsx
+++ gendocs-frontend/src/layout/PrivateLayout.tsx
@@ -1,18 +1,12 @@
-import { Paper } from "@mui/material";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Toolbar from "@mui/material/Toolbar";
 import * as React from "react";
-import { Routes, Route, Navigate } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import AppBar from "./components/AppBar";
 import Drawer from "./components/Drawer";
 import { DEFAULT_ROUTE, routes } from "./routes";
-
-const mdTheme = createTheme();
-
 
 const PrivateLayout = () => {
 
@@ -22,49 +16,46 @@ const PrivateLayout = () => {
     };
 
     return (
-        <ThemeProvider theme={mdTheme}>
-            <Box sx={{ display: "flex" }}>
-                <CssBaseline />
+        <Box sx={{ display: "flex" }}>
 
-                <AppBar open={open} toggleDrawer={toggleDrawer} />
-                <Drawer open={open} toggleDrawer={toggleDrawer} />
+            <AppBar open={open} toggleDrawer={toggleDrawer} />
+            <Drawer open={open} toggleDrawer={toggleDrawer} />
 
-                <Box
-                    component="main"
-                    sx={{
-                        backgroundColor: (theme) =>
-                            theme.palette.mode === "light"
-                                ? theme.palette.grey[100]
-                                : theme.palette.grey[900],
-                        flexGrow: 1,
-                        height: "100vh",
-                        overflow: "auto",
-                    }}
-                >
-                    <Toolbar />
-                    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-                        <Grid container spacing={3}>
-                            {/* Recent Orders */}
-                            <Grid item xs={12}>
-                                <Routes>
-                                    {
-                                        routes.map(item => (
-                                            <Route
-                                                key={item.path}
-                                                path={item.path}
-                                                element={item.component}
-                                            />
-                                        ))
-                                    }
+            <Box
+                component="main"
+                sx={{
+                    backgroundColor: (theme) =>
+                        theme.palette.mode === "light"
+                            ? theme.palette.grey[100]
+                            : theme.palette.grey[900],
+                    flexGrow: 1,
+                    height: "100vh",
+                    overflow: "auto",
+                }}
+            >
+                <Toolbar />
+                <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                    <Grid container spacing={3}>
+                        {/* Recent Orders */}
+                        <Grid item xs={12}>
+                            <Routes>
+                                {
+                                    routes.map(item => (
+                                        <Route
+                                            key={item.path}
+                                            path={item.path}
+                                            element={item.component}
+                                        />
+                                    ))
+                                }
 
-                                    <Route path="/" element={<Navigate to={DEFAULT_ROUTE} />} />
-                                </Routes>
-                            </Grid>
+                                <Route path="/" element={<Navigate to={DEFAULT_ROUTE} />} />
+                            </Routes>
                         </Grid>
-                    </Container>
-                </Box>
+                    </Grid>
+                </Container>
             </Box>
-        </ThemeProvider>
+        </Box>
     );
 };
 
