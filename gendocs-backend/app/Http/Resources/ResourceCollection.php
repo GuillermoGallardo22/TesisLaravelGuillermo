bdff5dc3baf5142bf772898d24016c997f8b282d
Luis Fernando Illapa <moxb@outlook.es>
2021-12-27 18:13:51 -0500

    feat(back): add resource

---
+++ gendocs-backend/app/Http/Resources/ResourceCollection.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection as BaseResourceCollection;
+
+class ResourceCollection extends BaseResourceCollection
+{
+    //public $collects = ResourceObject::class;
+
+    public function toArray($request)
+    {
+        return $this->collection;
+    }
+}
