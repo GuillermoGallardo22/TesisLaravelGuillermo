7c4c2b2b071149ff0230076ea50fd86eb5c1eb47
Luis Fernando Illapa <moxb@outlook.es>
2022-02-19 14:22:03 -0500

    refactor(front): update error summary component

--- gendocs-frontend/src/components/ErrorSummary.tsx
+++ gendocs-frontend/src/components/ErrorSummary.tsx
@@ -1,4 +1,4 @@
-import { Alert } from "@mui/material";
+import { Alert, Grid } from "@mui/material";
 import React from "react";
 
 interface ErrorSummaryProps {
@@ -9,11 +9,13 @@ const ErrorSummary: React.FC<ErrorSummaryProps> = ({ errors }) => {
     return !errors ? (
         <></>
     ) : (
-        <Alert severity="error">
-            {typeof errors === "string"
-                ? errors
-                : errors.map((error, index) => <p key={index}>{error}</p>)}
-        </Alert>
+        <Grid item xs={12}>
+            <Alert severity="error">
+                {typeof errors === "string"
+                    ? errors
+                    : errors.map((error, index) => <p key={index}>{error}</p>)}
+            </Alert>
+        </Grid>
     );
 };
 
