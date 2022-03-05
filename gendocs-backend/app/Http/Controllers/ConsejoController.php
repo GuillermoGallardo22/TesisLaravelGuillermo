e9000dccef98428aa4e3f8ba4596873a36430825
Luis Fernando Illapa <moxb@outlook.es>
2022-03-05 10:36:13 -0500

    feat(back): get counsils functionality
    - filter
    - paginate

--- gendocs-backend/app/Http/Controllers/ConsejoController.php
+++ gendocs-backend/app/Http/Controllers/ConsejoController.php
@@ -4,16 +4,34 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreConsejoRequest;
 use App\Http\Requests\UpdateConsejoRequest;
+use App\Http\Resources\ResourceCollection;
 use App\Http\Resources\ResourceObject;
 use App\Models\Consejo;
 use App\Models\Directorio;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class ConsejoController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $query = Consejo::query();
+
+        $query
+            ->orderBy('estado', 'DESC')
+            ->orderBy('fecha', 'DESC');
+
+        $query->applyFilters($request->all());
+
+        $paginate = $query->applyPaginate($request->all());
+
+        if ($paginate['isPageable']) {
+            return ResourceCollection::make(
+                $query->paginate($paginate['size'], '*', $paginate['param'], $paginate['number'])
+            );
+        }
+
+        return ResourceCollection::make($query->get());
     }
 
     public function store(StoreConsejoRequest $request)
--- gendocs-backend/app/Models/Consejo.php
+++ gendocs-backend/app/Models/Consejo.php
@@ -2,17 +2,26 @@
 
 namespace App\Models;
 
+use App\Traits\Filterable;
+use App\Traits\Pageable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Consejo extends Model
 {
-    use HasFactory;
+    use HasFactory, Filterable, Pageable;
 
     protected $fillable = [
         'nombre',
         'fecha',
         'tipo_consejo_id'
+    ];
+
+    public const FILTERS = ['search', 'estado'];
+
+    protected $casts = [
+        'estado' => 'boolean',
+        'fecha' => 'datetime'
     ];
 
     public function fields()
@@ -21,13 +30,23 @@ class Consejo extends Model
             'id' => $this->id,
             'nombre' => $this->nombre,
             'fecha' => $this->fecha,
-            'tipo_consejo' => $this->tipoConsejo(),
+            'tipo_consejo' => $this->tipoConsejo,
             'estado' => $this->estado,
         ];
     }
 
     public function tipoConsejo()
     {
-        return $this->belongsTo(TipoConsejo::class, 'tipo_consejo_id');
+        return $this->belongsTo(TipoConsejo::class, 'tipo_consejo_id', 'id');
+    }
+
+    public function scopeSearch($query, $target)
+    {
+        return $query->where('nombre', 'like', "%$target%");
+    }
+
+    public function scopeEstado($query, $target)
+    {
+        return $query->where('estado', '=', $target);
     }
 }
