307ae95c3e9e266af197c664f11662e7cb809868
Luis Fernando Illapa <moxb@outlook.es>
2022-03-04 22:35:00 -0500

    refactor(back): sort routes

--- gendocs-backend/routes/api.php
+++ gendocs-backend/routes/api.php
@@ -25,17 +25,15 @@ Route::middleware(["auth:sanctum", "isUserActive"])->group(function () {
 
     Route::apiResource('carreras', CarreraController::class);
     Route::apiResource('estudiantes', EstudianteController::class);
-
     Route::apiResource('procesos', ProcesoController::class);
-
     Route::apiResource('plantillas', PlantillasController::class);
 
+    // AUTH
     Route::get('roles', [RoleController::class, 'index']);
-
     Route::get('me', [UserController::class, 'me']);
     Route::apiResource('user', UserController::class);
 
-    //
+    // TEST
     Route::get('my-permissions', [GoogleDriveController::class, 'myPermissions']);
     Route::get('get-file', [GoogleDriveController::class, 'getFile']);
     Route::get('export-file', [GoogleDriveController::class, 'exportFile']);
