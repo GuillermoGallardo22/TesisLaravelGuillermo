8d7b59c280c044b74225459d74246240028ca4cc
Luis Fernando Illapa <moxb@outlook.es>
2022-03-05 10:36:46 -0500

    refactor(front): improve chip status component

--- gendocs-frontend/src/components/ChipStatus.tsx
+++ gendocs-frontend/src/components/ChipStatus.tsx
@@ -1,18 +1,35 @@
 import { Chip, ChipProps } from "@mui/material";
 
+type ChipColors =
+    | "default"
+    | "primary"
+    | "secondary"
+    | "error"
+    | "info"
+    | "success"
+    | "warning";
+
 interface ChipStatusProps extends ChipProps {
     value: boolean;
+    textPrimary?: string;
+    textSecondary?: string;
+    colorPrimary?: ChipColors;
+    colorSecondary?: ChipColors;
 }
 
 const ChipStatus: React.FC<ChipStatusProps> = ({
     children,
     value,
+    textPrimary = "Activado",
+    textSecondary = "Desactivado",
+    colorPrimary = "primary",
+    colorSecondary = "error",
     ...props
 }) => {
     return (
         <Chip
-            label={value ? "Activado" : "Desactivado"}
-            color={value ? "primary" : "error"}
+            label={value ? textPrimary : textSecondary}
+            color={value ? colorPrimary : colorSecondary}
             {...props}
         >
             {children}
