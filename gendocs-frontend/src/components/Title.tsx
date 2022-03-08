a1cae6b8a78873c41ed648c65f09ef9e5a5556a7
Luis Fernando Illapa <moxb@outlook.es>
2022-03-07 19:43:17 -0500

    style(front): update title component

--- gendocs-frontend/src/components/Title.tsx
+++ gendocs-frontend/src/components/Title.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Title: React.FC = ({ children }) => {
     return (
-        <Typography component="h2" variant="h6" color="primary">
+        <Typography component="h2" variant="h6">
             {children}
         </Typography>
     );
