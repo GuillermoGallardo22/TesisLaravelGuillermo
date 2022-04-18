3839d5925a478f034dc4ef5c329a1fd07cf44b64
Luis Fernando Illapa <moxb@outlook.es>
2022-04-17 19:05:33 -0500

    refactor(front): improve datagrid component

--- gendocs-frontend/src/components/DataGrid.tsx
+++ gendocs-frontend/src/components/DataGrid.tsx
@@ -4,5 +4,5 @@ import { ROW_PER_PAGE } from "utils";
 export const DataGrid: React.FunctionComponent<DataGridProps> = ({
   ...props
 }) => {
-  return <DataGridBase {...props} rowsPerPageOptions={ROW_PER_PAGE} />;
+  return <DataGridBase rowsPerPageOptions={ROW_PER_PAGE} {...props} />;
 };
