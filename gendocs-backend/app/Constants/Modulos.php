ba1bfa706ed90f1a6c812b08587efe220c9c20d1
Luis Fernando Illapa <moxb@outlook.es>
2022-05-21 17:05:18 -0500

    feat(back): add module codes

---
+++ gendocs-backend/app/Constants/Modulos.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Constants;
+
+abstract class Modulos
+{
+    /**
+     * SECRETARÍA DE FACULTAD
+     */
+    const FACU = 'FACU';
+
+    /**
+     * SECRETARÍA DE SUBDECANATO
+     */
+    const SUDE = 'SUDE';
+
+    /**
+     * SECRETARÍA DE TITULACIÓN
+     */
+    const TITU = 'TITU';
+}
