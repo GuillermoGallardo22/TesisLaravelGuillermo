6a23b28d67478a458128ad59a6ceee70f70df553
Luis Fernando Illapa <moxb@outlook.es>
2022-06-06 18:57:43 -0500

    refactor(front): update directories

--- gendocs-frontend/src/layout/components.ts
+++ gendocs-frontend/src/layout/components.ts
@@ -1,105 +1,115 @@
 import { lazy } from "react";
 
 export const ListResoluciones = lazy(
-  () => import("pages/private/consejos/components/ListResoluciones")
+  () => import("pages/private/facu/consejos/components/ListResoluciones")
 );
 
 export const AddDocente = lazy(
-  () => import("pages/private/docentes/components/AddDocente")
+  () => import("pages/private/facu/docentes/components/AddDocente")
 );
 
 export const UpdateDocente = lazy(
-  () => import("pages/private/docentes/components/UpdateDocente")
+  () => import("pages/private/facu/docentes/components/UpdateDocente")
 );
 
-export const Docentes = lazy(() => import("pages/private/docentes/Docentes"));
+export const Docentes = lazy(
+  () => import("pages/private/facu/docentes/Docentes")
+);
 
 export const AddCarrera = lazy(
-  () => import("pages/private/carreras/components/AddCarrera")
+  () => import("pages/private/facu/carreras/components/AddCarrera")
 );
 
-export const Carreras = lazy(() => import("pages/private/carreras/Carreras"));
+export const Carreras = lazy(
+  () => import("pages/private/facu/carreras/Carreras")
+);
 
 export const UpdateCarrera = lazy(
-  () => import("pages/private/carreras/components/UpdateCarrera")
+  () => import("pages/private/facu/carreras/components/UpdateCarrera")
 );
 
-export const Home = lazy(() => import("pages/private/home/Home"));
+export const Home = lazy(() => import("pages/private/facu/home/Home"));
 
 export const AddConsejo = lazy(
-  () => import("pages/private/consejos/components/AddConsejo")
+  () => import("pages/private/facu/consejos/components/AddConsejo")
 );
 
-export const Consejos = lazy(() => import("pages/private/consejos/Consejos"));
+export const Consejos = lazy(
+  () => import("pages/private/facu/consejos/Consejos")
+);
 
 export const Acta = lazy(
-  () => import("pages/private/consejos/components/Acta")
+  () => import("pages/private/facu/consejos/components/Acta")
 );
 
 export const ListMiembros = lazy(
-  () => import("pages/private/consejos/components/ListMiembros")
+  () => import("pages/private/facu/consejos/components/ListMiembros")
 );
 
 export const UpdateConsejo = lazy(
-  () => import("pages/private/consejos/components/UpdateConsejo")
+  () => import("pages/private/facu/consejos/components/UpdateConsejo")
 );
 
 export const AddDocumento = lazy(
-  () => import("pages/private/documentos/components/AddDocumento")
+  () => import("pages/private/facu/documentos/components/AddDocumento")
 );
 
 export const Documentos = lazy(
-  () => import("pages/private/documentos/Documentos")
+  () => import("pages/private/facu/documentos/Documentos")
 );
 
 export const AddProceso = lazy(
-  () => import("pages/private/procesos/components/AddProceso")
+  () => import("pages/private/facu/procesos/components/AddProceso")
 );
 
 export const AddPlantilla = lazy(
-  () => import("pages/private/procesos/components/AddPlantilla")
+  () => import("pages/private/facu/procesos/components/AddPlantilla")
 );
 
-export const Procesos = lazy(() => import("pages/private/procesos/Procesos"));
+export const Procesos = lazy(
+  () => import("pages/private/facu/procesos/Procesos")
+);
 
 export const ListPlantillas = lazy(
-  () => import("pages/private/procesos/components/ListPlantillas")
+  () => import("pages/private/facu/procesos/components/ListPlantillas")
 );
 
 export const UpdateProceso = lazy(
-  () => import("pages/private/procesos/components/UpdateProceso")
+  () => import("pages/private/facu/procesos/components/UpdateProceso")
 );
 
 export const UpdatePlantilla = lazy(
-  () => import("pages/private/procesos/components/UpdatePlantilla")
+  () => import("pages/private/facu/procesos/components/UpdatePlantilla")
 );
 
-export const Profile = lazy(() => import("pages/private/profile/Profile"));
+export const Profile = lazy(() => import("pages/private/facu/profile/Profile"));
 
 export const AddReserva = lazy(
-  () => import("pages/private/documentos/components/AddReserva")
+  () => import("pages/private/facu/documentos/components/AddReserva")
 );
 
 export const AddEstudiante = lazy(
-  () => import("pages/private/estudiantes/components/AddEstudiante")
+  () => import("pages/private/facu/estudiantes/components/AddEstudiante")
 );
 
 export const Estudiantes = lazy(
-  () => import("pages/private/estudiantes/Estudiantes")
+  () => import("pages/private/facu/estudiantes/Estudiantes")
 );
 
 export const UpdateEstudiante = lazy(
-  () => import("pages/private/estudiantes/components/UpdateEstudiante")
+  () => import("pages/private/facu/estudiantes/components/UpdateEstudiante")
 );
 
 export const AddUsuario = lazy(
-  () => import("pages/private/usuarios/components/AddUsuario")
+  () => import("pages/private/facu/usuarios/components/AddUsuario")
 );
 
-export const Usuarios = lazy(() => import("pages/private/usuarios/Usuarios"));
+export const Usuarios = lazy(
+  () => import("pages/private/facu/usuarios/Usuarios")
+);
 
 export const UpdateUsuario = lazy(
-  () => import("pages/private/usuarios/components/UpdateUsuario")
+  () => import("pages/private/facu/usuarios/components/UpdateUsuario")
 );
 
 export const DriveTemplate = lazy(() =>
