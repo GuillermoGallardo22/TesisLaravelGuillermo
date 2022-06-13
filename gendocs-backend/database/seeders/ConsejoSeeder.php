a4d65086ae9daadbb9a888bb31ceab3cac93e0ff
Luis Fernando Illapa <moxb@outlook.es>
2022-06-13 12:11:08 -0500

    refactor(back): implement module poly relationship on counsil seeders

--- gendocs-backend/database/seeders/ConsejoSeeder.php
+++ gendocs-backend/database/seeders/ConsejoSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Constants\Modulos;
 use App\Models\Consejo;
+use App\Models\Directorio;
+use App\Models\Module;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class ConsejoSeeder extends Seeder
@@ -14,6 +18,29 @@ class ConsejoSeeder extends Seeder
      */
     public function run()
     {
-        Consejo::factory(5)->create();
+        // Consejo::factory(5)->create();
+
+        $fecha = now()->addDays(2)->setHour(10)->setMinute(0);
+        $fFecha = $fecha->format("d/m/Y");
+
+        Consejo::create([
+            'nombre' => sprintf("Sesión %s %s", Modulos::FACU, $fFecha),
+            'fecha' => $fecha,
+            'tipo_consejo_id' => 1,
+            'directorio_id' => Directorio::activeDirectory()->id,
+            'estado' => true
+        ])->module()->create([
+            'module_id' => Module::where('code', Modulos::FACU)->first()->id
+        ]);
+
+        Consejo::create([
+            'nombre' => sprintf("Sesión %s %s", Modulos::SUDE, $fFecha),
+            'fecha' => $fecha,
+            'tipo_consejo_id' => 1,
+            'directorio_id' => Directorio::activeDirectory()->id,
+            'estado' => true
+        ])->module()->create([
+            'module_id' => Module::where('code', Modulos::SUDE)->first()->id
+        ]);
     }
 }
