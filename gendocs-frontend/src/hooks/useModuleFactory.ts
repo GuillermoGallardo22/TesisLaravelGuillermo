ae9d8252a77c209ee80020f6987333a4c539cc1b
Luis Fernando Illapa <moxb@outlook.es>
2022-06-21 19:44:43 -0500

    refactor(back): update error message

--- gendocs-frontend/src/hooks/useModuleFactory.ts
+++ gendocs-frontend/src/hooks/useModuleFactory.ts
@@ -3,18 +3,18 @@ import { ModuleEnum, PlantillasGlobales } from "models/enums";
 export function useModuleFactory(module: ModuleEnum) {
   const getPlantillasActa = (): PlantillasGlobales[] => {
     switch (module) {
-    case ModuleEnum.FACU:
-      return [
-        PlantillasGlobales.PLA_ACT_FACU,
-        PlantillasGlobales.PLA_ACT_SEP_FACU,
-      ];
-    case ModuleEnum.SUDE:
-      return [
-        PlantillasGlobales.PLA_ACT_SUDE,
-        PlantillasGlobales.PLA_ACT_SEP_SUDE,
-      ];
-    default:
-      throw new Error("");
+      case ModuleEnum.FACU:
+        return [
+          PlantillasGlobales.PLA_ACT_FACU,
+          PlantillasGlobales.PLA_ACT_SEP_FACU,
+        ];
+      case ModuleEnum.SUDE:
+        return [
+          PlantillasGlobales.PLA_ACT_SUDE,
+          PlantillasGlobales.PLA_ACT_SEP_SUDE,
+        ];
+      default:
+        throw new Error("Código de plantilla no implementado");
     }
   };
 
