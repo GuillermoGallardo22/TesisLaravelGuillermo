2f441e312f3df29f2186f52e495c32897ad2ab06
Luis Fernando Illapa <moxb@outlook.es>
2022-08-27 13:44:42 -0500

    feat(front): add constants values

--- gendocs-frontend/src/utils/constants.ts
+++ gendocs-frontend/src/utils/constants.ts
@@ -3,4 +3,6 @@ export const CONSTANTS = {
   email_gmail_regex: /^[a-z0-9](\.?[a-z0-9]){3,}@g(oogle)?mail\.com$/gm,
   phone_regex: /^[0-9]+$/gm,
   page_size: 100,
+  mayor_edad: 18,
+  fecha: "Y-MM-d", // FECHA NACIMIENTO
 };
