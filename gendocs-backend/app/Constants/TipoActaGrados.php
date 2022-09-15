e0fa9011e9dfae082acb0d40a289dada0fb0816b
Luis Fernando Illapa <moxb@outlook.es>
2022-09-14 22:28:38 -0500

    refactor(back): update codes

--- gendocs-backend/app/Constants/TipoActaGrados.php
+++ gendocs-backend/app/Constants/TipoActaGrados.php
@@ -6,12 +6,12 @@ abstract class TipoActaGrados
 {
 
     // TITULACIÓN
-    const T_EGCC = "T-EXAMEN-GRADO-CARACTER-COMPLEXIVO";
-    const T_PI = "T-PROYECTO-INVESTIGACIÓN";
-    const T_AA = "T-ARTÍCULO-ACADÉMICO";
+    const T_EGCC = "T-EGCC";
+    const T_PI = "T-PI";
+    const T_AA = "T-AA";
 
     // INTEGRACIÓN CURRICULAR
-    const IC_EGCC = "IC-EXAMEN-GRADO-CARACTER-COMPLEXIVO";
-    const IC_PI = "IC-PROYECTO-INVESTIGACIÓN";
-    const IC_AA = "IC-ARTÍCULO-ACADÉMICO";
+    const IC_EGCC = "IC-EGCC";
+    const IC_PI = "IC-PI";
+    const IC_AA = "IC-AA";
 }
