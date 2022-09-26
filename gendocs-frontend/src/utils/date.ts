237493335190989b45f8e67afacd3ed715640395
Luis Fernando Illapa <moxb@outlook.es>
2022-09-25 19:07:33 -0500

    feat(front): add util functions

--- gendocs-frontend/src/utils/date.ts
+++ gendocs-frontend/src/utils/date.ts
@@ -1,5 +1,9 @@
 import { format } from "date-fns";
 
+export function parseToDate(date: string) {
+  return format(new Date(date), "dd/MM/yyyy");
+}
+
 export function parseToDateTime(date: string) {
   return format(new Date(date), "dd/MM/yyyy HH:mm a");
 }
