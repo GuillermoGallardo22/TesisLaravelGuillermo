b3bb56e56eb00b2702f818e21c0e0ebadec7dde3
Luis Fernando Illapa <moxb@outlook.es>
2022-10-21 18:51:27 -0500

    refactor(back): update miembro acta grado table

---
+++ gendocs-backend/app/Constants/TipoAsistenteActaGrado.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Constants;
+
+abstract class TipoAsistenteActaGrado
+{
+    const TUTOR = "TUTOR";
+    const PRESIDENTE = "PRESIDENTE";
+    const M_PRINCIPAL = "M_PRINCIPAL";
+    const M_SUPLENTE = "M_SUPLENTE";
+}
--- gendocs-backend/database/migrations/2022_09_17_194630_create_miembros_acta_grados_table.php
+++ gendocs-backend/database/migrations/2022_09_17_194630_create_miembros_acta_grados_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Constants\TipoAsistenteActaGrado;
+use App\Constants\UniqueConstraintNames;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,20 +22,20 @@ class CreateMiembrosActaGradosTable extends Migration
                 ->references("id")
                 ->on("acta_grados");
 
-            $table->foreignId("docente_principal_id")
+            $table->foreignId("docente_id")
                 ->references("id")
                 ->on("docentes");
 
-            $table->foreignId("docente_suplente_id")
-                ->references("id")
-                ->on("docentes");
+            $table->enum('tipo', [TipoAsistenteActaGrado::TUTOR, TipoAsistenteActaGrado::M_PRINCIPAL, TipoAsistenteActaGrado::M_SUPLENTE, TipoAsistenteActaGrado::PRESIDENTE]);
 
-            $table->boolean("asistio_principal");
-            $table->boolean("asistio_suplente");
+            $table->string("informacion_adicional")->nullable();
 
-            $table->boolean("notificado_principal");
-            $table->boolean("notificado_suplente");
+            $table->boolean("asistio")->default(false);
+            $table->boolean("notificado")->default(false);
 
+            $table->unique(['acta_grado_id', 'docente_id'], UniqueConstraintNames::ACTA_GRADO_ACTA_GRADO_ID_DOCENTE_ID);
+
+            $table->softDeletes();
             $table->timestamps();
         });
     }
