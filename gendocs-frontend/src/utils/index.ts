422bc842e745aef2776145cf432216fe9464c6cc
Luis Fernando Illapa <moxb@outlook.es>
2022-10-22 14:54:32 -0500

    refactor(front): remove barrel files

--- gendocs-frontend/src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./axios";
-export * from "./constants";
-export * from "./date";
-export * from "./libs";
-export * from "./messages";
-export * from "./pagination";
-export * from "./theme";
