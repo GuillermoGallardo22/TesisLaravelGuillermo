2719458260064e1b837ffbf1b39edf57a0501284
Luis Fernando Illapa <moxb@outlook.es>
2022-11-29 23:30:45 -0500

    refactor(front): update format date

--- gendocs-frontend/src/utils/date.ts
+++ gendocs-frontend/src/utils/date.ts
@@ -1,7 +1,11 @@
 import { format } from "date-fns";
 
-export function parseToDate(date: string) {
-  return format(new Date(date), "dd/MM/yyyy");
+export function parseToDate(date: string | Date) {
+  if (typeof date === "string") {
+    date = new Date(date);
+  }
+
+  return format(date, "dd/MM/yyyy");
 }
 
 export function parseToDateTime(date: string) {
