4be9afc5bb3f94e6eb54760e361516294eff6789
Luis Fernando Illapa <moxb@outlook.es>
2022-12-15 23:05:58 -0500

    feat(back): add celdas notas tipo acta grado table

---
+++ gendocs-backend/app/Http/Controllers/CeldasNotasTipoActaGradoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCeldasNotasTipoActaGradoRequest;
+use App\Http\Requests\UpdateCeldasNotasTipoActaGradoRequest;
+use App\Models\CeldasNotasTipoActaGrado;
+
+class CeldasNotasTipoActaGradoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCeldasNotasTipoActaGradoRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCeldasNotasTipoActaGradoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CeldasNotasTipoActaGrado  $celdasNotasTipoActaGrado
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CeldasNotasTipoActaGrado $celdasNotasTipoActaGrado)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\CeldasNotasTipoActaGrado  $celdasNotasTipoActaGrado
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CeldasNotasTipoActaGrado $celdasNotasTipoActaGrado)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCeldasNotasTipoActaGradoRequest  $request
+     * @param  \App\Models\CeldasNotasTipoActaGrado  $celdasNotasTipoActaGrado
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCeldasNotasTipoActaGradoRequest $request, CeldasNotasTipoActaGrado $celdasNotasTipoActaGrado)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CeldasNotasTipoActaGrado  $celdasNotasTipoActaGrado
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CeldasNotasTipoActaGrado $celdasNotasTipoActaGrado)
+    {
+        //
+    }
+}
---
+++ gendocs-backend/database/migrations/2022_12_16_035929_create_celdas_notas_tipo_acta_grados_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCeldasNotasTipoActaGradosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('celdas_notas_tipo_acta_grados', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId("tipo_acta_grado_id")
+                ->references('id')
+                ->on('tipo_acta_grados');
+
+            $table->string("descripcion");
+            $table->string("celda");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('celdas_notas_tipo_acta_grados');
+    }
+}
