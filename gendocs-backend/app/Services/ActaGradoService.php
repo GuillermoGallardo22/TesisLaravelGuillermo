c5966ae6f3ad0f3330979f4b98f9bfb87f872462
Luis Fernando Illapa <moxb@outlook.es>
2022-12-18 23:50:03 -0500

    feat(back): apply notas to document

--- gendocs-backend/app/Services/ActaGradoService.php
+++ gendocs-backend/app/Services/ActaGradoService.php
@@ -44,6 +44,34 @@ class ActaGradoService
 
     public function generarDocumento(ActaGrado $actaGrado, TipoEstadoActaGrado $plantilla)
     {
+        // LEER CELDAS
+        $celdas = $plantilla->tipoActaGrado->celdasNotas->sortBy([
+            ["id", "asc"]
+        ]);
+
+        $notas = $celdas->map(function ($item) use ($actaGrado) {
+            $values = $this->googleDrive->getValues($actaGrado->documento_notas, "Sheet1!" . $item->celda);
+            $values = collect($values?->getValues() ?? [])->flatten();
+
+            $value = $values->count() >= 0 ? $values->toArray()[0] : 0;
+            $words = $this->converNumberToWords($value);
+
+            usleep(config("services.google.timeout_processing"));
+
+            return [
+                // "id" => $item->id,
+                // "celda" => $item->celda,
+                // "variable_nota" => $item->variable_nota,
+                // "variable_nota_texto" => $item->variable_nota_texto,
+                // "valor_nota" => $value,
+                // "valor_nota_texto" => $words,
+                "{{{$item->variable_nota}}}" => $value,
+                "{{{$item->variable_nota_texto}}}" => $words,
+            ];
+        })->collapse()->toArray();
+
+        // CREAR DOCUMENTO FINAL
+
         $documentoDrive = $this->googleDrive->copyFile(
             $actaGrado->estudiante->cedula . " | " . $actaGrado->tipo->codigo,
             Directorio::activeDirectory()->directorio_id,
@@ -53,6 +81,15 @@ class ActaGradoService
         $actaGrado->documento = $documentoDrive->id;
 
         $actaGrado->save();
+
+        // REEMPLAZAR
+
+        $this->googleDrive->replaceTextOnDocument(
+            array_merge(
+                $notas,
+            ),
+            $documentoDrive->id,
+        );
 
         return $actaGrado;
     }
